00b91bf6e2dfa5d26f5505f259f01437aba0200e
mspalacios2-lab <mspalacios2@espe.edu.ec>
2026-05-04 00:23:59 -0500

    Add files via upload

---
+++ IOPERACION_COLASYPILAS_GRUPO4.h
@@ -0,0 +1,8 @@
+#ifndef IOPERACION_COLASYPILAS_GRUPO4_H
+#define IOPERACION_COLASYPILAS_GRUPO4_H
+
+#include "Nodo.h"
+
+void imprimir(Nodo* inicio);
+
+#endif
---
+++ OPERACION_COLASYPILAS_GRUPO4.cpp
@@ -0,0 +1,29 @@
+#include "IOPERACION_COLASYPILAS_GRUPO4.h"
+#include <iostream>
+using namespace std;
+
+
+void imprimir(Nodo* inicio){
+
+    Nodo* actual = inicio;
+    int contElemento = 1;
+
+    if( inicio == nullptr )
+    {
+
+        cout << "La pila/cola esta vacia." << endl;
+        return;
+
+    }
+
+    while( actual != nullptr )  {
+
+     cout << "Elemento de la pila/cola: " << contElemento++ << endl;
+     cout << "Cedula: " << actual -> getCedula() << endl;
+     cout << "Nombre: " << actual -> getNombre() << endl;
+
+            actual = actual -> getSiguiente();
+    }
+
+
+}
