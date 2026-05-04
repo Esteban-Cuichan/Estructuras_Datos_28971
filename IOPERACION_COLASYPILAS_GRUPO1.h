3a0860344a8eef4a5e7975860d6406f07d9dba91
Eatan Ayala <ejayala2@espe.edu.ec>
2026-05-04 07:02:27 -0500

    Update IOPERACION_COLASYPILAS_GRUPO1.h

--- IOPERACION_COLASYPILAS_GRUPO1.h
+++ IOPERACION_COLASYPILAS_GRUPO1.h
@@ -2,7 +2,6 @@
 #define IOPERACION_COLASYPILAS_GRUPO1_H
 
 #include "Nodo.h"
-
-
-void insertarPila(Nodo*& cabeza, string cedula, string nombre);
+  void insertarPila(Nodo*& cabeza, string cedula, string nombre);
+  void insertarCola(Nodo*& cabeza, string cedula, string nombre);
 #endif
